01283a1d7ea80e4c2a8b657f54f76d4550cb9521
Loba007 <paulacious4greatness@gmail.com>
2025-11-14 07:59:51 +0100

    Innitial commit

---
+++ greet.py
@@ -0,0 +1,11 @@
+    def greet():
+    
+        print("Hey there")
+        print("Welcome," +  name)
+
+
+    #name = "Paul"
+    greet("Paul")
+    greet("Joseph")
+    greet("Blessing")
+    greet("Bukunmi")
